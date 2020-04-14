9ca40b602201594c756ef2f6a12b64e1b450e880
Mwansa <Mwansad.mwango@gmail.com>
2020-04-14 23:00:15 +0800

    Integrated github api query using username and repo name

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -8,38 +9,30 @@ function promptUser() {
   return inquirer.prompt([
     {
       type: "input",
-      name: "name",
-      message: "What is your name?"
-    },
-    {
-      type: "input",
-      name: "location",
-      message: "Where are you from?"
-    },
-    {
-      type: "input",
-      name: "hobby",
-      message: "What is your favorite hobby?"
-    },
-    {
-      type: "input",
-      name: "food",
-      message: "What is your favorite food?"
-    },
-    {
-      type: "input",
-      name: "github",
+      name: "githubUsername",
       message: "Enter your GitHub Username"
     },
     {
       type: "input",
-      name: "linkedin",
-      message: "Enter your LinkedIn URL."
-    }
+      name: "githubRepo",
+      message: "What is your project's Github repository?"
+    },
+    // To Do add other questions regarding project information
+    // { 
+    //   type: "input",
+    //   name: "description",
+    //   message: "What is your project's description?"
+    // },
+    // {
+    //   type: "input",
+    //   name: "installation",
+    //   message: "How does a user install the project?"
+    // },
+
   ]);
 }
-
-function generateHTML(answers) {
+// To DO Section below to be updated from HTML to MarkDown 
+function generateReadMe(answers) {
   return `
 <!DOCTYPE html>
 <html lang="en">
@@ -52,8 +45,8 @@ function generateHTML(answers) {
 <body>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
+    <h1 class="display-4">Hi! My name is ${answers.githubUsername}</h1>
+    <p class="lead">I am from ${answers.githubRepo}.</p>
     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
     <ul class="list-group">
       <li class="list-group-item">My GitHub username is ${answers.github}</li>
@@ -69,12 +62,16 @@ async function init() {
   console.log("hi")
   try {
     const answers = await promptUser();
+    const queryUrl = `https://api.github.com/repos/${answers.githubUsername}/${answers.githubRepo}`;
+    console.log(queryUrl);
+    
+    const response = await axios.get(queryUrl);
+    const githubRepoData = response.data;
+    console.log(githubRepoData);
 
-    const html = generateHTML(answers);
+    const forks = githubRepoData.forks;
+    console.log(forks);
 
-    await writeFileAsync("index.html", html);
-
-    console.log("Successfully wrote to index.html");
   } catch(err) {
     console.log(err);
   }
