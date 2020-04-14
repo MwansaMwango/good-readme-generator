c4ad0a77029a52e8bf9bd4ac1e30b5519bd84b7b
Mwansa <Mwansad.mwango@gmail.com>
2020-04-14 20:26:46 +0800

    Initial index.js baseline commit.

--- index.js
+++ index.js
@@ -1,60 +1,83 @@
-const fs = require ('fs').promises;
-const inquirer = require ('inquirer');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-inquirer
-    .prompt ([
-        {
-            type:'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type:'input',
-            name: 'location',
-            message: 'What suburb do you live in?',
-        },
-        {
-            type:'input',
-            name: 'bio',
-            message: 'Tell us about yourself?',
-        },
-        {
-            type:'input',
-            name: 'linkedin',
-            message: 'What is your linkedIn URL?',
-        },
-        {
-            type:'input',
-            name: 'github',
-            message: 'What is your github URL?',
-        }
-     ])
-        .then(({name, location, bio, linkedin, github }) => {
-            const profileHTML = `
-            <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>${name}</title>
-                </head>
-                <body>
-                    <h2>${name}</h2>
-                    <h3>Location: ${location}</h3>
-                    <p>${bio}</p>
-                    <p>LinkedIn: ${linkedin}</p>
-                    <p>GitHub: ${github}</p>
-                </body>
-            </html>`;
-           return fs.writeFile('output.html', profileHTML);
-        
-        }).then (
-            console.log('File Written!')
-        )
-          .catch(error => {
-            if(error.isTtyError) {
-              // Prompt couldn't be rendered in the current environment
-            } else {
-              console.error(error)
-            }
-         });
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "location",
+      message: "Where are you from?"
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?"
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
+    }
+  ]);
+}
+
+function generateHTML(answers) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
+
+async function init() {
+  console.log("hi")
+  try {
+    const answers = await promptUser();
+
+    const html = generateHTML(answers);
+
+    await writeFileAsync("index.html", html);
+
+    console.log("Successfully wrote to index.html");
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+init();
