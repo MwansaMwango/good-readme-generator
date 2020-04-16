e661868a86a7d37e0b1e751ccb877fbc70e32aa2
Mwansa <Mwansad.mwango@gmail.com>
2020-04-16 21:39:25 +0800

    Updated questions for readme creation and error handling

--- index.js
+++ index.js
@@ -10,62 +10,78 @@ function promptUser() {
     {
       type: "input",
       name: "githubUsername",
-      message: "Enter your GitHub Username"
+      message: "What is your GitHub Username"
     },
     {
       type: "input",
       name: "githubRepo",
-      message: "What is your project's Github repository?"
+      message: "What is your Github project repository?"
     },
-    // To Do add other questions regarding project information
-    // { 
-    //   type: "input",
-    //   name: "description",
-    //   message: "What is your project's description?"
-    // },
-    // {
-    //   type: "input",
-    //   name: "installation",
-    //   message: "How does a user install the project?"
-    // },
-
+    { 
+      type: "input",
+      name: "description",
+      message: "Enter project's description?"
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter project installation instruction?"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Enter project usage?"
+    },
+    {
+      type: "input",
+      name: "licence",
+      message: "Enter project licence details?"
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "How can others contribute to the project?"
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Enter project testing details?"
+    },
+    {
+      type: "input",
+      name: "githubEmail",
+      message: "Enter contact email for questions?"
+    },
   ]);
 }
 // To DO Section below to be updated from HTML to MarkDown 
 function generateReadMe(answers) {
+/**badges:
+ * repos 
+data.update_at
+data.language
+data.forks
+data.watchers
+data.open_issues
+data.owner.avatar_url
+ * users
+data.email 
+**/
   return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.githubUsername}</h1>
-    <p class="lead">I am from ${answers.githubRepo}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+  # project-title \n
+
+
+`;
 }
 
 async function init() {
-  console.log("hi")
+
   try {
     const answers = await promptUser();
-    const queryUrl = `https://api.github.com/repos/${answers.githubUsername}/${answers.githubRepo}`;
-    console.log(queryUrl);
+    const queryRepoUrl = `https://api.github.com/repos/${answers.githubUsername}/${answers.githubRepo}`;
+    console.log(queryRepoUrl);
     
-    const response = await axios.get(queryUrl);
+    let response = await axios.get(queryRepoUrl);
     const githubRepoData = response.data;
     console.log(githubRepoData);
 
@@ -73,7 +89,8 @@ async function init() {
     console.log(forks);
 
   } catch(err) {
-    console.log(err);
+    console.error(err.response.statusText);
+
   }
 }
 
