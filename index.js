5f0504ad49cbc200fd2434d1b1463c274e9ba366
Mwansa <Mwansad.mwango@gmail.com>
2020-04-18 09:20:33 +0800

    Cleaned up code and added comments

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
   return inquirer.prompt([
+    // Get readme details of new repository from user
     {
       type: "input",
       name: "githubUsername",
@@ -59,82 +60,36 @@ function promptUser() {
     },
   ]);
 }
-// To DO Section below to be updated from HTML to MarkDown 
+
 async function generateReadMe() {
-/**badges:
- * repos 
-data.update_at
-data.language
-data.forks
-data.watchers
-data.open_issues
-data.owner.avatar_url
- * users
-data.email 
-**/
-
-// let readMeData = `
-// # ${answers.projectTitle}
-// [![Github Update](${updatedBadgeUrl})]
-// [![Github Forks](${forksBadgeUrl})]
-
-// ## Decription
-// ${answers.decription}
-
-// ## Installation
-// ${answers.installation}
-// ## Usage
-// ${answers.usage}
-
-// ## Licence
-// ${answers.licence}
-
-// ## Contribution
-// ${answers.contribution}
-
-// ## Tests
-// ${answers.tests}
-
-// ## Have any questions?
-// ![Github profile picture](${profPicUrl})
-// [Github Link](${githubUrl})
-// [Email](${email})
-// `;
-// writeFileAsync ('MyReadME.md', readMeData,(err, data => {
-// if (err) throw err;
-// console.log ("Successfully created readme file");  
-// }))
-
-}
-
-async function init() {
 
   try {
+    // Get Answers from User prompts
     const answers = await promptUser();
+
+    //Github API request
     const queryRepoUrl = `https://api.github.com/repos/${answers.githubUsername}/${answers.githubRepo}`;
     console.log(queryRepoUrl);
-    
     const response = await axios.get(queryRepoUrl);
     const githubRepoData = response.data;
-    // const updatedAt = githubRepoData.updated_at;
-    // .split("T")[0];
+ 
+    // Badge Data: Updated at
     let updatedAt = new Date (githubRepoData.updated_at);
     updatedAt = updatedAt.getDate()+'-' + (updatedAt.getMonth()+1) + '-'+updatedAt.getFullYear();
     console.log(updatedAt);
     const updatedBadgeUrl = `https://img.shields.io/static/v1?label=Updated%20at&message=${updatedAt}&color=blue&?style=social&logo=github`
     console.log(updatedBadgeUrl);
     
+    // Badge Data: Forks
     const forks = githubRepoData.forks;
     const forksBadgeUrl = `https://img.shields.io/static/v1?label=Forks&message=${forks}&color=green&?style=social&logo=github`
     console.log(forksBadgeUrl);
     
-    
+    // Get user data from Github API
     const profPicUrl = githubRepoData.owner.avatar_url;
     const githubUrl = githubRepoData.owner.url;
-    const email = githubRepoData.email;
     
     // Generate readme content data
-
     let readMeData = 
 `
 # ${answers.projectTitle} - ${answers.githubRepo}
@@ -143,9 +98,19 @@ async function init() {
 
 ## Description
 ${answers.decription}
-
+---
+##Table of Contents  
+​
+ * [Installation](#installation)
+ * [Usage](#usage)
+ * [Licence](#licence)
+ * [Contributing](#contributing)
+ * [Tests](#tests)
+ * [Questions](#questions) 
+--- 
 ## Installation
 ${answers.installation}
+
 ## Usage
 ${answers.usage}
 
@@ -157,22 +122,22 @@ ${answers.contribution}
 
 ## Tests
 ${answers.tests}
-
+---
 ## Have any questions?
 ![Github profile picture](${profPicUrl})
 * [Github Link](${githubUrl})
-* [Email](${email})
+* [Email](${answers.email})
 `;
+
     //Write to new readme file
     writeFileAsync ('MyReadME.md', readMeData, (err) => {
     if (err) throw err;
-    console.log ("Successfully created readme file");  
     });
-
+    console.log ("Successfully created 'MyReadME.md' file");  
+    //Handle rejected promise
   } catch(err) {
-    console.error(err);
-
+    console.error(err.message);
   }
 }
 
-init();
+generateReadMe();
