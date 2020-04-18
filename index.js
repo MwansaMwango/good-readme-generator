d2fa7ce5a9fca7aa491a16013009ba5270b8898c
Mwansa <Mwansad.mwango@gmail.com>
2020-04-18 19:22:26 +0800

    Cleaned code to remove comments and whitespace

--- index.js
+++ index.js
@@ -68,31 +68,26 @@ async function generateReadMe() {
 
     //Github API request
     const queryRepoUrl = `https://api.github.com/repos/${answers.githubUsername}/${answers.githubRepo}`;
-    console.log(queryRepoUrl);
     const response = await axios.get(queryRepoUrl);
     const githubRepoData = response.data;
  
     // Badge Data: Updated at
     let updatedAt = new Date (githubRepoData.updated_at);
     updatedAt = updatedAt.getDate()+'-' + (updatedAt.getMonth()+1) + '-'+updatedAt.getFullYear();
-    console.log(updatedAt);
     const updatedBadgeUrl = `https://img.shields.io/static/v1?label=Updated%20at&message=${updatedAt}&color=yellow&?style=social&logo=github`
-    console.log(updatedBadgeUrl);
-    
+
     // Badge Data: Forks
     const forks = githubRepoData.forks;
     const forksBadgeUrl = `https://img.shields.io/static/v1?label=Forks&message=${forks}&color=green&?style=social&logo=github`
-    console.log(forksBadgeUrl);
     
     // Badge Data: Stars
     const stars = githubRepoData.stargazers_count;
     const starsBadgeUrl = `https://img.shields.io/static/v1?label=stars&message=${stars}&color=blue&?style=social&logo=github`
-    console.log(starsBadgeUrl);
 
     // Badge Data: Issues
     const openIssues = githubRepoData.open_issues;
     const openIssuesBadgeUrl = `https://img.shields.io/static/v1?label=open%20issues&message=${openIssues}&color=orange&?style=social&logo=github`
-    console.log(openIssuesBadgeUrl);
+
     
     // Get user data from Github API
     const profPicUrl = githubRepoData.owner.avatar_url;
@@ -102,33 +97,27 @@ async function generateReadMe() {
     let readMeData = 
 `
 # ${answers.projectTitle} ![Github Update](${updatedBadgeUrl}) ![Github Forks](${forksBadgeUrl}) ![Github Forks](${starsBadgeUrl}) ![Github Forks](${openIssuesBadgeUrl})
-
 ## Description
-${answers.decription}
+${answers.description}
 
 ---
-##Table of Contents  
-​
+## Table of Contents
+
  * [Installation](#installation)
  * [Usage](#usage)
  * [Licence](#licence)
  * [Contributing](#contributing)
  * [Tests](#tests)
  * [Questions](#questions) 
-
 ---
 ## Installation
 ${answers.installation}
-
 ## Usage
 ${answers.usage}
-
 ## Licence
 ${answers.licence}
-
 ## Contribution
 ${answers.contribution}
-
 ## Tests
 ${answers.tests}
 
